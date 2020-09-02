11ae939cc7063a73bd08eed229a1cd8a3d80d51c
Sergio Hernandez Castillo <shcastillo@MacBook-Pro.lan>
2020-09-02 17:32:31 -0500

    Se añadieron datos generales del ejercicio y un struct para un proceso.

--- Ejercicio_1/main.c
+++ Ejercicio_1/main.c
@@ -1,4 +1,15 @@
+/*
+Autór: Sergio Hernandez Castillo
+Matrícula: A01025210
+Descripción: Actividad 3 - Administración de un SO - Ejercicio 1
+*/
+
 #include <stdio.h>
+
+typedef struct {
+    int id;
+    int tiempoDeEjecucion;
+} Proceso;
 
 int main(){
     printf("Hello World!");
